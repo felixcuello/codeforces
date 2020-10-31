1ffed89e3f6889abc32b4cd84a1f729676ed2049
Felix Cuello <felix.cuello@gmail.com>
2020-10-31 00:10:33 -0300

    [CPP:Codeforces] Added my template.cc

---
+++ codeforces/template.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <math.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define __sieve(n) vector<long> __sieve_mtx(n, true); vector<long> __primes; \
+  for(long p=2; p<n;++p) { \
+    if(__sieve_mtx[p] == false) continue; \
+    __primes.push_back(p); \
+    for(long i=p*p; i<n; i+=p) \
+      __sieve_mtx[i] = false; \
+  }
+
+using namespace std;
+
+int main() {
+//  ifstream in;
+//  ofstream out;
+
+//  in.open("input.txt");
+//  out.open("output.txt");
+
+  return 0;
+}
