1864869dbb806370a9f94c44d95d62a2d254958a
Felix Cuello <felix.cuello@gmail.com>
2020-11-29 18:54:11 -0300

    [CPP:Codeforces] Refactored template.cc

--- codeforces/template.cc
+++ codeforces/template.cc
@@ -7,15 +7,7 @@
 #include <math.h>
 #include <unistd.h>
 #include <limits.h>
-
-#define ceil(x,y) ((x + y - 1) / y)
-
-#define pb push_back
-#define pob push_back
-#define mp make_pair
-#define fore(i,a,b) for(int i=a,to=b;i<to;++i)
-
-/* limits.h
+/* liits.h
  * LONG_MIN   -2147483647
  * LONG_MAX   2147483647
  * ULONG_MAX  4294967295
@@ -24,16 +16,16 @@
  * ULLONG_MAX 18446744073709551615
  */
 
+#define ceil(x,y) ((x + y - 1) / y)
+#define fore(i,a,b) for(int i=a,to=b;i<to;++i)
+#define __sieve(n) vector<long> __sieve_mtx(n, true); vector<long> __primes; for(long p=2; p<n;++p) { if(__sieve_mtx[p] == false) continue; __primes.push_back(p); for(long i=p*p; i<n; i+=p) __sieve_mtx[i] = false; }
+
+#define pb push_back
+#define pob push_back
+#define mp make_pair
+
 typedef long long ll;
 typedef unsigned long long ull;
-
-#define __sieve(n) vector<long> __sieve_mtx(n, true); vector<long> __primes; \
-  for(long p=2; p<n;++p) { \
-    if(__sieve_mtx[p] == false) continue; \
-    __primes.push_back(p); \
-    for(long i=p*p; i<n; i+=p) \
-      __sieve_mtx[i] = false; \
-  }
 
 using namespace std;
 
