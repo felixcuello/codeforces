404f87e07fdb9e19f45bba05c5e89264e8e03f3d
Felix Cuello <felix.cuello@gmail.com>
2021-01-10 06:44:04 -0300

    [GoLang] Added some math helper functions for ints

--- template.go
+++ template.go
@@ -4,6 +4,15 @@ import (
   "fmt"
 )
 
+//////////////////////////////////////////////////////////////////////////
+// Helper functions
+//////////////////////////////////////////////////////////////////////////
+func iMin(a int, b int) int { if a<b { return a }; return b }
+func iMax(a int, b int) int { if a<b { return b }; return a }
+func i64Min(a int64, b int64) int64 { if a<b { return a }; return b }
+func i64Max(a int64, b int64) int64 { if a<b { return b }; return a }
+//////////////////////////////////////////////////////////////////////////
+
 func solve() {
 }
 
